4b3e096443d8e271a1cb2002064f746b6d1f7ad8
Carson 723 <23cadememy@gmail.com>
2022-08-08 21:40:13 -0500

    Put four lists in main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+destinations_list = ['Punta Cana', 'Turks and Caicos', 'Greenland', 'Sweden', 'United Kingdom']
+restaurants_list = ['Mexican', 'Italian', 'Greek', 'Lebanese', 'Caribbean']
+mode_of_transport_list = ['Train', 'Car', 'Bus', 'Boat', 'Moped'] 
+entertainment_list = ['Circus', 'Concert', 'Adventure Park', 'Movies', 'Magic show']
